9b858f0c83532429ecbbc8b3f7d02a129dc77247
k-shunji <k-shunji@flect.co.jp>
2014-03-12 12:44:18 +0900

    Add index.html

--- jquery.animateDialog.js
+++ jquery.animateDialog.js
@@ -11,8 +11,8 @@
 		"iteration-count" : 1,
 		"duration" : "3s",
 		"timing-function" : "ease",
-		"delay" : 0,
-		"direction" : "alternate"
+		"delay" : "0s",
+		"direction" : "normal"
 	};
 
 	function MessageDialog($el) {
@@ -28,60 +28,81 @@
 			return parseInt(value) * 1000;
 
 		}
-	    function show(msg, options) {
-	    	var params = {};
-	    	if (!options) {
-	    		options = {};
-	    	}
-	    	for (var i=0; i<properties.length; i++) {
-	    		var name = properties[i],
-	    			value = null;
-	    		if (options[name]) {
-	    			value = options[name];
-	    		} else if (initials[name]) {
-	    			value = initials[name];
-	    		} else {
-	    			value = defaults[name];
-	    		}
-	    		params["animation-" + name] = value;
-	    		params["-webkit-animation-" + name] = value;
-	    	}
-    		if (msg) {
-    			$el.find(".message").text(msg);
-	    	}
-console.log(JSON.stringify(params));
-    		$el.css(params);
-    		$el.show();
-    		setTimeout(function() {
-    			$el.hide();
-	        }, durationToMillis(params["animation-duration"]));
-    	}
-    	var initials = {};
-    	for (var i=0; i<properties.length; i++) {
-    		var name = properties[i],
-    			value = $el.css("animation-" + name);
-    		if (value) {
-    			initials[name] = value;
-    		}
-    	}
-	    $.extend(this, {
-    	  "show" : show
-    	})
-    }
+		function show(options) {
+			if (shown) {
+				queue.push(options);
+				return;
+			}
+			shown = true;
+			var params = {},
+				msg = options.message;
+			for (var i=0; i<properties.length; i++) {
+				var name = properties[i],
+					value = null;
+				if (options[name]) {
+					value = options[name];
+				} else if (initials[name]) {
+					value = initials[name];
+				} else {
+					value = defaults[name];
+				}
+				params["animation-" + name] = value;
+				params["-webkit-animation-" + name] = value;
+			}
+			if (msg) {
+				$el.find(".message").text(msg);
+			}
+			$el.css(params);
+			$el.show();
+			setTimeout(function() {
+				$el.hide();
+				if (initialText) {
+					$el.find(".message").text(initialText);
+				}
+				shown = false;
+				if (queue.length > 0) {
+					var next = queue.shift();
+					setTimeout(function() {
+						show(next);
+					}, 10);
+				}
+			}, durationToMillis(params["animation-duration"]));
+		}
+		var initials = {},
+			initialText = $el.find(".message").text(),
+			shown = false,
+			queue = [];
+		for (var i=0; i<properties.length; i++) {
+			var name = properties[i],
+				value = $el.css("animation-" + name);
+			if (value && value !== "none" && value !== "0s") {
+				initials[name] = value;
+			}
+		}
+		$.extend(this, {
+		  "show" : show
+		})
+	}
 
-    $.fn.animateDialog = function(msg, options) {
-    	if (this.length == 0) {
-    		return;
-    	}
-    	if (typeof(msg) === "object") {
-    		options = msg;
-    		msg = null;
-    	}
-    	var dlg = $.data(this[0], "animateDialog");
-    	if (!dlg) {
-    		dlg = new MessageDialog(this);
-    		$.data(this[0], "animateDialog", dlg);
-    	}
-    	dlg.show(msg, options);
-    }
+	$.fn.animateDialog = function(msg, options) {
+		if (this.length == 0) {
+			return;
+		}
+		if (typeof(msg) === "object") {
+			options = msg;
+			msg = null;
+		}
+		if (!options) {
+			options = {};
+		}
+		if (msg) {
+			options.message = msg;
+		}
+		var dlg = $.data(this[0], "animateDialog");
+		if (!dlg) {
+			dlg = new MessageDialog(this);
+			$.data(this[0], "animateDialog", dlg);
+		}
+		dlg.show(options);
+	}
 })(jQuery);
